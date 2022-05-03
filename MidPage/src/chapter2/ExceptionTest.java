c85b69496698634fb3c30b39a0720afccfad4ebe
Mars Lee <MarsLee5@foxmail.com>
2022-05-03 14:28:33 +0800

    220503

---
+++ MidPage/src/chapter2/ExceptionTest.java
@@ -0,0 +1,53 @@
+package chapter2;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+/*指令8:异常处理*/
+public class ExceptionTest {
+    public void throwZero(int i) {
+        if (i == 0) {
+            throw new RuntimeException(" 参数值为0");
+        }
+    }
+
+    public void throw0ne(int i) throws RuntimeException {
+        if (i == 1) {
+            throw new RuntimeException(" 参数值为1");
+        }
+    }
+
+    public void throwArithmetic() {
+        int i = 10;
+        int j = i / 0;
+        System.out.println(j);
+    }
+
+    public void tryCatch() {
+        try {
+            File file = new File("d: /hello.txt");
+            FileInputStream fis = new FileInputStream(file);
+            String info = "hello!";
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (RuntimeException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //思考:如下方法返回结果为多少?
+    public static String func() {
+        String str = "hello";
+        try {
+            return str;
+        } finally {
+            str = "atguigu";
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(func());//hello
+    }
+
+}
---
+++ MidPage/src/chapter2/SynchronizedTest.java
@@ -0,0 +1,18 @@
+package chapter2;
+
+/*指令9:同步控制指令*/
+public class SynchronizedTest {
+    private int i = 0;
+
+    public void add() {
+        i++;
+    }
+
+    private Object obj = new Object();
+
+    public void subtract() {
+        synchronized (obj) {
+            i--;
+        }
+    }
+}
